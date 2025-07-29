db356a9e7973e3ecaab25f0a68ef04bfc36847cf
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-07-29 23:27:17 +0530

    Create Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import  Home from "./Pages/Home";
-import  About  from "./Pages/About";
-import  Profile  from "./Pages/Profile";
-import  SignIn  from "./Pages/SignIn";
-import  SignUp  from "./Pages/SignUp";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Header from "./Components/Header";
 
 export const App = () => {
   return (
-   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/sign-in" element={<SignIn/>}/>
-      <Route path="/sign-up" element={<SignUp/>}/>
-      <Route path="/profile" element={<Profile/>}/>
-      <Route path="/about" element={<About/>}/>
-    </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+    </BrowserRouter>
   );
 };
