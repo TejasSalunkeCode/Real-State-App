ed1c769ba7002807d353bfe8e00015cc20e52fce
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-08-02 23:36:40 +0530

    Add redux persist

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,12 @@
-export const errorHandler=(statuscode,message)=>{
-    const error = new Error();
-    error.statuscode=statuscode;
-    error.message=message;
-    return error;
+// export const errorHandler=(statuscode,message)=>{
+//     const error = new Error();
+//     error.statuscode=statuscode;
+//     error.message=message;
+//     return error;
+// };
+
+export const errorHandler = (statusCode, message) => {
+  const error = new Error(message);
+  error.statusCode = statusCode;
+  return error;
 };
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,28 @@
 // src/app/store.js
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import  userReducer  from "./user/userSlice.js";
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { persistStore } from "redux-persist";
 // import counterReducer from "../features/counter/counterSlice";
 
+const rootReducer = combineReducers({user : userReducer});
+
+const persistConfig={
+  key:'root',
+  storage,
+  version:1,
+}
+
+const persistedReducer = persistReducer(persistConfig,rootReducer);
+
 export const store = configureStore({
-  reducer: {user:userReducer },
+  reducer: persistedReducer,
     // counter: counterReducer,
     middleware:(getDefaultMiddleware) => getDefaultMiddleware({
       serializableCheck:false,
     }),
-  
 });
+
+
+export const persistor = persistStore(store);
