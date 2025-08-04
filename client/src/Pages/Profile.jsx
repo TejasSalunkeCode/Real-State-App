116ae583af85a69e0dd0ccaf30fcf1f6219f2ee4
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-08-05 00:28:45 +0530

    complete update user functionality

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -119,14 +119,26 @@
 
 import React, { useRef, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import {updateUserFailure,updateUserSuccess,updateUserStart} from "../redux/user/userSlice";
+import { useDispatch } from 'react-redux';
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser,loading,error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [uploadProgress, setUploadProgress] = useState(0);
   const [formData, setFormData] = useState({});
+  const [update,setupdateSuccess]=useState(false);
+//   const [formData, setFormData] = useState({
+//   username: currentUser.username,
+//   email: currentUser.email,
+//   avatar: currentUser.avatar,
+// });
+
   const [fileUploadError, setFileUploadError] = useState(false);
+  const dispatch=useDispatch();
+  console.log(formData);
+  
 
   useEffect(() => {
     if (file) {
@@ -165,10 +177,36 @@ export default function Profile() {
     }
   };
 
+  const handleChange=(e)=>{
+    setFormData({...formData,[e.target.id] : e.target.value})
+  }
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res= await fetch(`/api/user/update/${currentUser._id}`,{
+         method: 'POST',
+        headers:{
+          'Content-Type' : 'application/json'
+        },
+        body:JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success===false){
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data))
+      setupdateSuccess(true);
+     } catch (error) {
+      dispatch(updateUserFailure(error.message))
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type='file'
@@ -191,17 +229,21 @@ export default function Profile() {
         {uploadProgress === 100 && !fileUploadError && (
           <p className='text-green-600 text-sm text-center'>Image uploaded successfully!</p>
         )}
-        <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' />
-        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' />
-        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' />
-        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>
-          Update
+        <input type='text' placeholder='username' defaultValue={currentUser.username}  className='border p-3 rounded-lg' id='username' onChange={handleChange} />
+
+        <input type='email' placeholder='email' defaultValue={currentUser.email} className='border p-3 rounded-lg' id='email'  onChange={handleChange}/>
+
+        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
+        <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading ? 'Loading...' : 'Update'}
         </button>
       </form>
       <div className='flex justify-between mt-5'>
         <span className='text-red-700 cursor-pointer'>Delete account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
+      <p className='text-red-700 mt-5'>{error ? error : ' '}</p>
+      <p className='text-green-700 mt-5'>{update ? 'user updated successfully!' : ' '}</p>
     </div>
   );
 }
